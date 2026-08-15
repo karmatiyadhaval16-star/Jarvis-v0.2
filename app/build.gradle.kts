967ff05b169c21202af812b0cc9f90eb77c07fc7
karmatiyadhaval16-star <karmatiyadhaval16@gmail.com>
2026-08-15 10:05:07 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.Jarvis.assistant"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.Jarvis.assistant"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
